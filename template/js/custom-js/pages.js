94be4c531d4a31e95695c6570b1cda45318e21be
matheusgnreis <matheus@e-com.club>
2023-06-28 20:27:41 -0300

    chore(layout): step 5.0 product page

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+$('#search-input').on('input', (e) => {
+    console.log(e.currentTarget.value)
+    $('.search__box.card').show()
+    $('.search__input.form-control').val(e.currentTarget.value).trigger('input')
+})
