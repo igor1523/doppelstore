d249dd620a550157d46b9d442aa2ae66ad834ac3
Leonardo Matos <leomp120894@gmail.com>
2023-06-30 13:04:37 -0300

    Copying `TheProduct` component source [skip ci]

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,8 @@ module.exports = () => ({
       './html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/html/TheAccount.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
+      './html/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js')
     }
   }
 })
